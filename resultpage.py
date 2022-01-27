e048675773a70d62d9a8c9f99afc5acc27202abf
Igor Mendes <igordesouzamendes@outlook.com>
2022-01-26 23:05:47 -0300

    |Patch| Reorganizacao de Funcoes

--- resultpage.py
+++ resultpage.py
@@ -6,7 +6,7 @@ def main():
     top = tk.Toplevel()
     top.title("Resultado da Pesquisa")
 
-    global X
+    global X    
     x = 0
     global data
     global value
@@ -21,17 +21,19 @@ def main():
     label01 = tk.Label(top, text="Indice", anchor='w')
     label01.grid(row=0, column=0)
 
-    label02 = tk.Label(top, text="Problema", anchor='w')
-    label02.grid(row=0, column=2)
+    label02 = tk.Label(top, text="Dificuldade", anchor='w')
+    label02.grid(row=0, column=1)
 
-    label03 = tk.Label(top, text="Link", anchor='w')
-    label03.grid(row=0, column=3)
+    label03 = tk.Label(top, text="Problema", anchor='w')
+    label03.grid(row=0, column=2)
 
-    label03 = tk.Label(top, text="Dificuldade", anchor='w')
-    label03.grid(row=0, column=1)
+    label04 = tk.Label(top, text="Link", anchor='w')
+    label04.grid(row=0, column=3)
+
 
     for i in banco_de_dados:
         x = x + 1
+
         indice = tk.Label(top, text=i[0], anchor='w')
         indice.grid(row=x, column=0)
 
@@ -43,5 +45,4 @@ def main():
 
         difc = tk.Label(top, text=i[3], anchor='w')
         difc.grid(row=x, column=1)
-
 
